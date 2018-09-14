0f0877de5fecebb1ce841c61a9f2c359bce1928c
T-101 <T-101@PC226>
2018-09-13 19:44:56 -0500

    Mi primer proyecto de github

---
+++ src/garcia/ariel/GarciaAriel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package garcia.ariel;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author T-101
+ */
+public class GarciaAriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // System.out.println("hola mundo");
+        JOptionPane.showConfirmDialog(null, " Hola mundo");
+        // TODO code application logic here
+    }
+    
+}
